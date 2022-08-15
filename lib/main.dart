9f57e6f02eb3e75093dc558dc779c82e0f0dc9ed
Manish Goyal <manishgoyal016@gmail.com>
2022-08-15 21:12:00 +0530

    Added the iOS version

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,14 @@ import '../providers/team_id.dart';
 import '../screens/team_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
